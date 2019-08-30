2d5acfb18ffc0f7f333bca35411594a512e12e66
tjcjc <taijcjc@gmail.com>
2019-08-30 18:06:37 +0800

    update .gitignore

--- Example/Pods/Target Support Files/JTUtils/JTUtils-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_JTUtils : NSObject
-@end
-@implementation PodsDummy_JTUtils
-@end
--- Example/Pods/Target Support Files/MDictParser/MDictParser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MDictParser : NSObject
-@end
-@implementation PodsDummy_MDictParser
-@end
