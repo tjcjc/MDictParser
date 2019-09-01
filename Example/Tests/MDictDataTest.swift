0cc41ff997f74b72865c01c71cf95afa2d8ca6da
tjcjc <taijcjc@gmail.com>
2019-09-01 13:34:58 +0800

    add more test

--- Example/Tests/MDictDataTest.swift
+++ Example/Tests/MDictDataTest.swift
@@ -65,31 +65,31 @@ class TableOfContentsSpec: QuickSpec {
 //                expect("time").toEventually( equal("done") )
 //            }
 //
-//            context("these will pass") {
-//
-//                it("can do maths") {
-//                    expect(23) == 23
-//                }
-//
-//                it("can read") {
-//                    expect("🐮") == "🐮"
-//                }
-//
-//                it("will eventually pass") {
-//                    var time = "passing"
-//
-//                    DispatchQueue.main.async {
-//                        time = "done"
-//                    }
-//
-//                    waitUntil { done in
-//                        Thread.sleep(forTimeInterval: 0.5)
-//                        expect(time) == "done"
-//
-//                        done()
-//                    }
-//                }
-//            }
+            context("these will pass") {
+
+                it("can do maths") {
+                    expect(23) == 23
+                }
+
+                it("can read") {
+                    expect("🐮") == "🐮"
+                }
+
+                it("will eventually pass") {
+                    var time = "passing"
+
+                    DispatchQueue.main.async {
+                        time = "done"
+                    }
+
+                    waitUntil { done in
+                        Thread.sleep(forTimeInterval: 0.5)
+                        expect(time) == "done"
+
+                        done()
+                    }
+                }
+            }
         }
     }
 }
